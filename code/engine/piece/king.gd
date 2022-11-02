663f58a321096f256a38ddd69a9b9569aebe9df2
Adam Wong <tue91717@temple.edu>
2022-11-02 19:23:38 -0400

    Add king piece and its basic movements.

---
+++ code/engine/piece/king.gd
@@ -0,0 +1,49 @@
+extends Piece
+# King represents the king chess piece.
+
+
+# Cardinal directions
+var directions = [[0, 1], [1,1], [1, 0], [1, -1],
+		[0, -1], [-1, -1], [-1, 0], [-1, 1]]
+
+
+func _init(team_new):
+	team = team_new
+	type = Type.KING
+	hasMoved = false
+
+
+func calc_moves(orig_pos, max_pos, board_width, board_map, _prev_move):
+	var moves = []
+
+	# Get basic movements.
+	for dir in directions:
+		var position = orig_pos + dir[0] + dir[1] * board_width
+		if _is_move_valid(position, max_pos, board_map):
+			moves.append(position)
+
+	# TODO Implement castling validation.
+	return moves
+
+# TODO add public method to search for check given any board state.
+
+
+# Checks if a space can be moved into.
+# Arguments:
+# position: The prospective position.
+# max_pos: The maximum position.
+# board_map: The map of board positions and chess pieces.
+# Return:
+#   True is the move is valid, else false.
+func _is_move_valid(position, max_pos, board_map):
+	if (
+			position >= 0
+			and position <= max_pos
+			and (
+					not board_map.has(position)
+					or board_map[position].team != team and board_map[position].type != Type.KING
+			)
+	):
+		return true
+	else:
+		return false
