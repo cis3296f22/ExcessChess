fc725c2f965f351376cab8fc611b19a1abc77a7b
Adam Wong <tue91717@temple.edu>
2022-11-05 19:35:31 -0400

    Modify knight into custom piece with knight move.

---
+++ code/engine/move/knight_move.gd
@@ -0,0 +1,39 @@
+# LinearMove contains the logic for a knight's movements.
+
+
+# Movement vectors.
+var movements = [[1, 2], [-1, 2]]
+# Cardinal rotations
+var rotations = [[1, 0, 0, 1], [0, -1, 1, 0], [-1, 0, 0, -1], [0, 1, -1, 0]]
+
+
+# Properties
+var name
+
+
+func _init(name_):
+	name = name_
+
+
+# Validates squares for a knight's move and adds them to the list of positions.
+# Arguments:
+# positions: The array of positions.
+# game: The game state information.
+# pos: The original position of the piece.
+# piece: The selected piece's state information.
+func add_pos(positions, game, pos, piece):
+	var pieces = game.pieces
+	for move in movements:
+		for rot in rotations:
+			var new_pos = (
+					pos
+					+ move[0] * rot[0]
+					+ move[1] * rot[1]
+					+ (move[0] * rot[2] + move[1] * rot[3]) * game.width
+			)
+			if (
+					new_pos >= 0
+					and new_pos <= game.max_pos
+					and (not pieces.has(new_pos) or pieces[new_pos].team != piece.team)
+			):
+				positions.append(new_pos)
--- code/engine/piece/knight.gd
+++ code/engine/piece/knight.gd
@@ -1,33 +1,10 @@
-# Knight contains the knight chess piece logic.
+extends "res://engine/piece/custom_piece.gd"
+# Knight stores the knight chess piece logic.
 
 
-var movements = [[1, 2], [-1, 2]]
-# Cardinal rotations
-var rotations = [[1, 0, 0, 1], [0, -1, 1, 0], [-1, 0, 0, -1], [0, 1, -1, 0]]
+var KnightMove = preload("res://engine/move/knight_move.gd")
 
 
-# Given game state information, this function returns a list of potentially valid new positions.
-# Arguments:
-# pos: The original position. of the piece.
-# piece: The piece's state information.
-# game: The game's state information.
-# Return:
-#	Returns an array of positions.
-func calc_moves(pos, piece, game):
-	var positions = []
-	var pieces = game.pieces
-	for move in movements:
-		for rot in rotations:
-			var new_pos = (
-					pos
-					+ move[0] * rot[0]
-					+ move[1] * rot[1]
-					+ (move[0] * rot[2] + move[1] * rot[3]) * game.get("width")
-			)
-			if (
-					new_pos >= 0
-					and new_pos <= game.get("max_pos")
-					and (not pieces.has(new_pos) or pieces[new_pos].team != piece.team)
-			):
-				positions.append(new_pos)
-	return positions
+func _init():
+	# Add the default knight moves.
+	add_move(KnightMove.new("knight jump"))
