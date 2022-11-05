352b5eb3d5d4a0fbc49bc14716276b40d4b7a1be
OptimalFailure <54728770+OptimalFailure@users.noreply.github.com>
2022-11-05 18:40:33 -0400

    Moved more control to the Board scene. Adjusted the craftingmenu scene accordingly. Added highlights to the board.

--- code/crafting_menu/PieceDrawer.gd
+++ code/crafting_menu/PieceDrawer.gd
@@ -1,9 +1,10 @@
 tool
 extends Container
 
-
+#Todo: inherit sizes from board
 export var DEFUALT_WIDTH = 8
 export var DEFUALT_HEIGHT = 8
+
 var contents = [] #.resize(DEFUALT_WIDTH*DEFUALT_HEIGHT)
 
 var green_square = preload("res://assets/board/green_square.png")
@@ -11,10 +12,9 @@ var pawn = preload("res://assets/pieces/white_pawn.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	print(pawn.get_height())
 	var tile_size = Vector2(green_square.get_width(), green_square.get_height())
 	var piece_size = Vector2(pawn.get_width(),pawn.get_height())
-	#The ammount of space to include on each size of the piece, to center it in a square
+	#The amount of space to include on each size of the piece, to center it in a square
 	var piece_size_side_gap = (tile_size.x - piece_size.x)/2 #NOTE: This will likely cause issues if the gap is odd 
 	#The hight to shift the piece up to make the bottom flush with the bottom of the square (instead of tops flush)
 	var height_shift = (tile_size.y - piece_size.y)
@@ -23,21 +23,15 @@ func _ready():
 	for _i in range(DEFUALT_HEIGHT):
 		for _j in range(DEFUALT_WIDTH):
 			var x = TextureRect.new()
-			x.texture = green_square
-			x.set_modulate(Color(1,1,1,0)) #Set alpha to 0, so it's invisible
-			x.set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
-#			x.set_id( (i*DEFUALT_WIDTH) + j)
-#			x.texture_hover = green_square
-			contents.append(x)
-			add_child(x)
-			x.rect_position = Vector2(tile_size.x *_j + piece_size_side_gap, tile_size.y*_i + height_shift)
+			x.texture = null
+			x.set_mouse_filter(Control.MOUSE_FILTER_IGNORE) #Allow clicks on the button below
+			contents.append(x) #Save to list of children
+			add_child(x) #And add it to scene tree
+			x.rect_position = Vector2(tile_size.x *_j + piece_size_side_gap, tile_size.y*_i + height_shift) #The apply the shift
 
 func show_child(child:int, tex):
 	contents[child].texture = tex
-	contents[child].set_modulate(Color(1,1,1,1))
 	
-#func child_pressed(bound):
-#	print("Child ", bound, " Pressed!")
 
 func as_string():
 	var result = ""
@@ -48,7 +42,8 @@ func as_string():
 #	print("Result: ",result)
 	for piece in get_children():
 		if piece.texture == null:
-			pass
+			#Blank space
+			result += "_ "
 		else:
 			var tex = piece.texture.load_path
 			if("white_pawn" in tex):
@@ -76,7 +71,7 @@ func as_string():
 				result += "bk "
 			elif("black_queen" in tex):
 				result += "bq "
-		#Blank / unknown
+		#unknown
 			else:
 				result += "_ "
-	print("Result: ", result)
+	return result
--- code/crafting_menu/PiecePainter.gd
+++ code/crafting_menu/PiecePainter.gd
@@ -1,9 +1,25 @@
 tool #Allow it to work in editor
 extends Control
 
-#Other stuff will likely go here eventually
+signal tile_clicked(tile)
 
-#func _ready():
-#	pass # Replace with function body.
+#This node just manages and delegates from the children
+
+#Echo the signals from the buttons
+func _ready():
+	$SquareSelector.connect("tile_clicked",self,"tile_selected")
+#cont.
+func tile_selected(tile):
+	emit_signal("tile_clicked",tile)
+
+#Set a pice to a specific texture
+func set_piece(tile, piece):
+	$PieceDrawer.show_child(tile, piece)
+
+#Get the pieces and board size as a string
+func as_string():
+	return $PieceDrawer.as_string()
 
 
+func highlight_square(child:int, color:Color = Color(1,1,0,0.75)):
+	$HighlightTiles.highlight_square(child, color)
