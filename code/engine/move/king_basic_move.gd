216df2f398b12ad64922a9b2b5ae2154087b2d18
Adam Wong <tue91717@temple.edu>
2022-11-05 21:04:34 -0400

    Modify king into custom piece and factor out its basic moves.

---
+++ code/engine/move/king_basic_move.gd
@@ -0,0 +1,41 @@
+# KingBasicMove contains the logic for a one-tile movement in eight directions.
+
+
+# Cardinal directions
+var directions = [[0, 1], [1,1], [1, 0], [1, -1],
+		[0, -1], [-1, -1], [-1, 0], [-1, 1]]
+
+
+# Properties
+var name
+var can_capture
+var must_capture
+
+
+func _init(name_, can_capture_ = true, must_capture_ = false):
+	name = name_
+	can_capture = can_capture_
+	must_capture = must_capture_
+
+
+# Validates a line of spaces and adds valid spaces to the list of positions.
+# Arguments:
+# positions: The array of positions.
+# game: The game state information.
+# pos: The original position of the piece.
+# piece: The selected piece's state information.
+func add_pos(positions, game, pos, _piece):
+	var new_pos
+	var pieces = game.pieces
+	for dir in directions:
+		new_pos = pos + dir[0] + dir[1] * game.width
+
+		if (
+			new_pos >= 0
+			and new_pos <= game.max_pos
+			and (
+				(pieces.has(new_pos) and can_capture)
+				or (not pieces.has(new_pos) and not must_capture)
+			)
+		):
+			positions.append(new_pos)
--- code/engine/piece/king.gd
+++ code/engine/piece/king.gd
@@ -1,42 +1,12 @@
+extends "res://engine/piece/custom_piece.gd"
 # King contains the king chess piece logic.
 
 
-# Cardinal directions
-var directions = [[0, 1], [1,1], [1, 0], [1, -1],
-		[0, -1], [-1, -1], [-1, 0], [-1, 1]]
+var KingBasicMove = preload("res://engine/move/king_basic_move.gd")
 
 
-# Given game state information, this function returns a list of potentially valid new positions.
-# Arguments:
-# pos: The original position. of the piece.
-# piece: The piece's state information.
-# game: The game's state information.
-# Return:
-#	Returns an array of positions.
-func calc_moves(pos, piece, game):
-	var positions = []
+func _init():
+	# Add the default king moves.
+	add_move(KingBasicMove.new("1-tile 8-directions"))
 
-	# Get basic movements.
-	for dir in directions:
-		var new_pos = pos + dir[0] + dir[1] * game.get("width")
-		_add_move(positions, new_pos, piece.team, game.get("max_pos"), game.pieces)
-
-	# TODO Implement castling validation.
-
-	return positions
-
-
-# Checks if a space can be moved into, and if so, adds it to the list.
-# Arguments:
-# positions: The array of positions to append to.
-# new_pos: The prospective position.
-# max_pos: The maximum board position.
-# team: The piece's team.
-# pieces: The map of board positions and chess pieces.
-func _add_move(positions, new_pos, max_pos, team, pieces):
-	if (
-			new_pos >= 0
-			and new_pos <= max_pos
-			and (not pieces.has(new_pos) or pieces[new_pos].team != team)
-	):
-		positions.append(new_pos)
+	# TODO Add kingside and queenside castling validation.
